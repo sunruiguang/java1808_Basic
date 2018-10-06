c0924b9622ee252c1ac433997db10087a485b115
sunruiguang <srg.dandelion@gmail.com>
2018-10-06 11:41:50 +0800

    tests

--- src/com/test/Test01.java
+++ src/com/test/Test01.java
@@ -1,10 +1,17 @@
 package com.test;
 
-public class Test01 {
-	public static void main(String args[]) {
-		StringBuilder sb = new StringBuilder("aa");
-		sb.append("ss");
-		System.out.println(sb);
-	}
+import java.util.HashSet;
+import java.util.Set;
 
+public class Test01 {
+    public static void main(String args[]) {
+        Set<String> set = new HashSet<>();
+        set.add("zs");
+        set.add("ls");
+        set.add("sun");
+        System.out.println("hello");
+        for (String i : set) {
+            System.out.println(i);
+        }
+    }
 }
